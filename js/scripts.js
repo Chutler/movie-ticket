d46f5c9bacdf74a85331ab19de6578586e6b652e
Craig Hutler and Mike McShane <craig@>
2019-07-01 20:35:21 -0700

    begin building js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
-// Business Logic for Contacts ---------
-function Ticket(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.phoneNumber = phoneNumber
+// Business Logic for Contacts
+
+
+function Ticket(movieTitle, movieTime, age) {
+  this.movieTitle = movieTitle,
+  this.movieTime = movieTime,
+  this.age = age
 }
+
+
+// user interface logic
+
+$(document).ready(function() {
+  $("form#movies").submit(function(event) {
+    event.preventDefault();
+
+    var movieTitle = ($("#movieTitle").val())
+    var movieTime = ($("#movieTime").val())
+    var age = ($("#age").val())
+    console.log(newTicket);
+  });
+});
+
+var newTicket = new Ticket(movieTitle, movieTime, age);
