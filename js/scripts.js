d8b9939047c84ed53d8477ae454cc478d7539632
Craig Hutler and Mike McShane <mike@>
2019-07-01 20:16:48 -0700

    setup the initial skeleton

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Business Logic for Contacts ---------
+function Ticket(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
